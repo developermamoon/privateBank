164f62ee3fa3b1758fe007deb28e9c697b8f66cd
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2022-08-20 17:26:07 +0600

    almost complete deposit

--- deposit.js
+++ deposit.js
@@ -14,6 +14,14 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     const previousDepositValue = parseFloat(previousDepositValueString);
 
     // step3 updating new deposit value and clearning the value in the input field
-    previousDeposit.innerText = previousDepositValue+UserDepositAmountValue;
+    previousDeposit.innerText = previousDepositValue + UserDepositAmountValue; //calculation part
     UserDeposit.value ='';
+
+    //step 4 adding the deposit money to total balance
+    const totalBalance = document.getElementById('total-balance');
+    const totalBalanceValueString= totalBalance.innerText; 
+    // converting to number
+    const totalBalanceValue = parseFloat(totalBalanceValueString);
+    const totalBalanceNew = totalBalanceValue+UserDepositAmountValue; //calculation part
+    totalBalance.innerText= totalBalanceNew;
 })
